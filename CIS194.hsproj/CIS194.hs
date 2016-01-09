dddad1dc85dcd502347e173c5c998b09d8a5f41e
Brian Johnson <brian@brianjohnson.co.za>
2016-01-09 13:28:50 +0200

    Up to exercise 2 on week 1.

---
+++ CIS194.hsproj/CIS194.hs
@@ -0,0 +1,22 @@
+--Exersize 1 - Credit Card Vaildation
+
+toDigits :: Integer -> [Integer]
+toDigitsRev :: Integer -> [Integer]
+doubleEveryOther :: [Integer] -> [Integer]
+
+toDigits x
+  | x <= 0 = []
+  | otherwise = (toDigits (x `div` 10)) ++ [(x `mod` 10)]
+
+toDigitsRev x = reverse (toDigits x)
+
+doubleEveryOtherRev [] = []
+doubleEveryOtherRev [x] = [x]
+doubleEveryOtherRev (x:y:zs) = 
+  x : y*2 : doubleEveryOther zs
+
+doubleEveryOther x = (reverse (doubleEveryOtherRev (reverse x)))
+
+sumDigits x:xs
+  |x > 10 = sumDigits ((x `div` 10) : (x `mod` 10)) + sumDigits xs
+  |otherwise = x + sumDigits xs
